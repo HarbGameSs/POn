fcfe0781aaacb3a8d4a48b2912e3e5304f778743
HarbGameSs <132205462+HarbGameSs@users.noreply.github.com>
2023-04-30 15:34:05 +0300

    psx
    psx

---
+++ Comet.lua
@@ -0,0 +1,5 @@
+MassiveComet = true
+MiniComet = true
+WebHook = "https://discordapp.com/api/webhooks/1102207890623827978/DGsfLZBLyK9oY7bSQAVATy6yxHQ7nUT2Or4B_xgqzYge4OPNFEtt0MCKq5V8DMfI4voo"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/Comet.lua", true))()
